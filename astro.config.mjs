dfa0a3866aefbe4c58482578588dbb70fadef079
Eoban <zwick.fabi@googlemail.com>
2024-05-25 18:18:24 +0200

    fixed md formatting. tested some mdx features

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), compress()],
+  integrations: [tailwind(), icon(), compress(), mdx()],
   output: "server",
   adapter: vercel()
 });
---
+++ src/pages/test.md
@@ -0,0 +1,3 @@
+# this is a test header
+- yo
+1. yoyo
