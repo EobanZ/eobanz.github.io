0ee23c38b132c5aac7322d8dac2c0bb2b245011c
Eoban <zwick.fabi@googlemail.com>
2024-05-28 08:07:24 +0200

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress(), mdx()],
-  output: "static",
-
+  site: 'https://eobanz.github.io',
 });
